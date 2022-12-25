0cd84e1ca6708eb4d3bfd24a0ee337eab62b8533
Mariahelenaterto <109740578+Mariahelenaterto@users.noreply.github.com>
2022-12-25 18:46:42 -0300

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -10,7 +10,7 @@
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 
 
--- What is the total amount each customer spent at the restaurant? 
+-- 1. What is the total amount each customer spent at the restaurant? 
 WITH TOTAL_AMOUNT AS (SELECT *
 FROM dannys_diner.sales s
 inner join dannys_diner.menu m on s.product_id = m.product_id)
@@ -19,11 +19,12 @@ FROM TOTAL_AMOUNT
 GROUP BY customer_id
 ORDER BY customer_id;
 
-| customer_id   |      sum      |
+|  customer_id  |      sum      |
 | ------------- | ------------- |
 |       A       |       76      |
 |       B       |       74      |
 |       C       |       36      |
+
 
 -- 2. How many days has each customer visited the restaurant?
 SELECT customer_id,
@@ -31,8 +32,8 @@ COUNT(DISTINCT(order_date)) AS DAYS_VISITED
 FROM dannys_diner.sales
 GROUP BY customer_id;
 
--- 3. What was the first item from the menu purchased by each customer?
 
+-- 3. What was the first item from the menu purchased by each customer?
 WITH first_item AS 
 (SELECT DISTINCT order_date,customer_id, product_id
  FROM dannys_diner.sales)
@@ -44,7 +45,6 @@ WITH first_item AS
  
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
-
 WITH top_item AS
 (SELECT *
 FROM dannys_diner.sales s
@@ -57,8 +57,8 @@ GROUP BY product_name
 ORDER BY total_count DESC
 LIMIT 1;
 
--- 5. Which item was the most popular for each customer?
 
+-- 5. Which item was the most popular for each customer?
 WITH top_item_customer AS
 (SELECT *
 FROM dannys_diner.menu m
@@ -74,8 +74,8 @@ SELECT product_name, customer_id
 FROM top_item_two
 WHERE ranking = 1;
 
--- 6. Which item was purchased first by the customer after they became a member?
 
+-- 6. Which item was purchased first by the customer after they became a member?
 WITH item_sales AS
 (SELECT s.customer_id, s.order_date, s.product_id, m.join_date
 FROM dannys_diner.sales s
@@ -93,5 +93,38 @@ INNER JOIN dannys_diner.menu m ON r.product_id = m.product_id
 WHERE ranker=1
 ORDER BY customer_id;
 
+
 -- 7. Which item was purchased just before the customer became a member?
+WITH item_sales AS
+(SELECT s.customer_id, s.order_date, s.product_id, m.join_date
+FROM dannys_diner.sales s
+INNER JOIN dannys_diner.members m ON s.customer_id = m.customer_id),
+
+item_ranked AS
+(SELECT customer_id, order_date, product_id, join_date, 
+RANK() OVER(PARTITION BY customer_id ORDER BY order_date DESC) as ranker
+FROM item_sales
+WHERE order_date<join_date)
+
+SELECT customer_id, order_date, join_date, product_name
+FROM item_ranked r
+INNER JOIN dannys_diner.menu m ON r.product_id = m.product_id
+WHERE ranker=1
+ORDER BY customer_id;
+
+
+-- 8. What is the total items and amount spent for each member before they became a member?
+
+WITH item_sales AS
+(SELECT s.customer_id, s.order_date, s.product_id, n.price, n.product_name
+FROM dannys_diner.sales s
+INNER JOIN dannys_diner.members m ON s.customer_id = m.customer_id
+INNER JOIN dannys_diner.menu n ON n.product_id = s.product_id
+WHERE order_date<join_date)
+
+SELECT customer_id, COUNT(customer_id), SUM(price)
+FROM item_sales
+GROUP BY customer_id
+ORDER BY customer_id;
+
 
